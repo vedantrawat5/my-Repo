61ba40c95b1d1fe4ceafe246758c75ed9b0e23c9
Vedant <vedantrawat5@gmail.com>
2024-05-02 10:03:59 +0530

    Project-11 Status: Complete

--- Project-11/script.js
+++ Project-11/script.js
@@ -1,29 +1,11 @@
-let DobStatus =false;
-let dateOfBirth;
-
-const settingWheelEl = document.getElementById("settingIcon");
-const settingContentEl = document.getElementById("settingContent");
-const initialTextEl =document.getElementById("initialText");
-const afterDOBBtnTxtEl =document.getElementById("afterDOBBtnTxt");
-const dobButtonEl =document.getElementById("dobButton");
-
-
-const toggleDateOfBirthSelector = () =>{
-    if(DobStatus){
-        settingContentEl.classList.add('hide');
+function changeColor(){
+    var hex_numbers = ["0","1", "2","3","4","5","6","7","8","9","A","B","C","D","E","F"];
+    var hex_code  = '';
+    
+    for(var i = 0; i<6; i++){
+        var random_index = Math.floor(Math.random() * hex_numbers.length);
+        hex_code += hex_numbers[random_index];
     }
-    else{
-        settingContentEl.classList.remove('hide');
-    }
-    DobStatus =!DobStatus;
-
-    console.log('Toggle',DobStatus);
-};
-
-const setDOBHandler = () => {
-    dateOfBirth = dobButtonEl.value;
-    console.log('date of birth', dateOfBirth);  
-};
-
-settingWheelEl.addEventListener('click', toggleDateOfBirthSelector);
-dobButtonEl.addEventListener('click', toggleDateOfBirthSelector);
+    document.getElementById("hex_code").innerHTML = hex_code;
+    document.getElementsByTagName("body")[0].style.background = "#"+ hex_code; 
+}   
